0f4d5e78d3e11ca9d0c020f1902614c380fde1fc
= <marcjosephcruz24@gmail.com>
2025-12-10 10:30:58 +0800

    add header component

---
+++ src/component/Header/Logo.jsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <p className="text-2xl font-semibold tracking-tight">
+        <span className="text-violet-600 font-poppins">we</span>
+        <span className="text-black">findways</span>
+      </p>
+    </div>
+  );
+};
+
+export default Logo;
