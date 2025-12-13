31ce457aa5955b5f5c399d4ddeb06d603388b625
= <marcjosephcruz24@gmail.com>
2025-12-13 22:11:41 +0800

    feat: add category filter component for products

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,35 +1,41 @@
-  const ProductCard = ({ product }) => {
-    return (
-      <div className="w-full h-full max-w-full">
-        <div className="group bg-white rounded-2xl shadow-sm hover:shadow-xl border border-gray-100 transition-all duration-300 overflow-hidden flex flex-col">
-          {/* Product Image */}
-          <div className="relative w-full h-64 bg-gray-100 overflow-hidden">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="w-full h-full object-contain p-4 group-hover:scale-110 transition-transform duration-500"
-            />
-          </div>
 
-          {/* Product Details */}
-          <div className="p-5 flex flex-col grow">
-            <h2 className="text-lg font-semibold text-gray-900 mb-2 line-clamp-1">
-              {product.name}
-            </h2>
 
-            <div className="flex justify-between items-center mt-auto">
-              <span className="text-xl font-bold text-pink-600">
-                ${product.price}
-              </span>
+const ProductCard = ({ filteredProducts }) => {
+  return (
+    <>
+      {filteredProducts.map((product, index) => (
+        <div key={index} className="w-full h-full max-w-full">
+          <div className="group bg-white rounded-2xl shadow-sm hover:shadow-xl border border-gray-100 transition-all duration-300 overflow-hidden flex flex-col">
+            {/* Product Image */}
+            <div className="relative w-full h-64 bg-gray-100 overflow-hidden">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full h-full object-contain p-4 group-hover:scale-110 transition-transform duration-500"
+              />
+            </div>
 
-              <span className="text-sm bg-pink-100 text-pink-600 px-3 py-1 rounded-full capitalize">
-                {product.category}
-              </span>
+            {/* Product Details */}
+            <div className="p-5 flex flex-col grow">
+              <h2 className="text-lg font-semibold text-gray-900 mb-2 line-clamp-1">
+                {product.name}
+              </h2>
+
+              <div className="flex justify-between items-center mt-auto">
+                <span className="text-xl font-bold text-pink-600">
+                  ${product.price}
+                </span>
+
+                <span className="text-sm bg-pink-100 text-pink-600 px-3 py-1 rounded-full capitalize">
+                  {product.category}
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  };
+      ))}
+    </>
+  );
+};
 
-  export default ProductCard;
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,55 @@ import { Plus } from "lucide-react";
 import { useState } from "react";
 import { useProduct } from "../hooks/ProductContext";
 import dynamicProducts from "../hooks/useDynamic";
+import Category from "../component/Category";
 
 const Home = () => {
   const [open, setOpen] = useState(false);
   const { product, handleAddProduct } = useProduct();
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   const combinedProducts = [...dynamicProducts, ...product];
 
+  const filteredProducts =
+    selectedCategory === "All"
+      ? combinedProducts
+      : combinedProducts.filter(
+          (product) =>
+            product.category.toLowerCase() === selectedCategory.toLowerCase()
+        );
+
   return (
     <Layout>
-      {/* Modal */}
       <div className="min-h-screen bg-gray-100 py-10 px-10 md:px-25">
+        <Category
+          combinedProducts={combinedProducts}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        {/* Product card(s) */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 xl:gap-15 place-items-center">
+          <ProductCard
+            combinedProducts={combinedProducts}
+            setOpen={setOpen}
+            filteredProducts={filteredProducts}
+          />
+
+          <div
+            onClick={() => setOpen(true)}
+            className={`${
+              selectedCategory === "All"
+                ? "group h-90 w-full bg-white rounded-2xl shadow-sm hover:shadow-xl border border-dashed border-pink-400 transition-all duration-300 flex items-center justify-center cursor-pointer"
+                : "hidden"
+            }`}
+          >
+            <Plus
+              size={40}
+              className="text-pink-500 group-hover:scale-110 group-active:scale-100 transition"
+            />
+          </div>
+        </div>
+
+        {/* Modal */}
         {open && (
           <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
             <Modal
@@ -25,23 +63,6 @@ const Home = () => {
             />
           </div>
         )}
-
-        {/* Product card(s) */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 xl:gap-15 place-items-center">
-          {combinedProducts.map((product, index) => (
-            <ProductCard key={index} product={product} setOpen={setOpen} />
-          ))}
-
-          <div
-            onClick={() => setOpen(true)}
-            className="group h-90 w-full bg-white rounded-2xl shadow-sm hover:shadow-xl border border-dashed border-pink-400 transition-all duration-300 flex items-center justify-center cursor-pointer"
-          >
-            <Plus
-              size={40}
-              className="text-pink-500 group-hover:scale-110 group-active:scale-100 transition"
-            />
-          </div>
-        </div>
       </div>
     </Layout>
   );
