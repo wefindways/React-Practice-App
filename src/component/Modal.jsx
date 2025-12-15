1b5e53c239798249e6404cde06a39150cf1dc591
Mac <marcjosephcruz24@gmail.com>
2025-12-15 10:51:55 +0800

    Merge pull request #3 from wefindways/feat/add-product
    * feat: add dynamic images
    
    * feat: add product list component
    
    * feat: add custom hooks using useContext for shared state management
    
    * refactor: rename productsData() to useDynamic() for clearer hook naming
    
    * feat: update and rename ProductCard component
    
    * feat: add custom hook for managing product form state and submission
    
    * feat: implement modal form for adding products to the list
    
    * feat: implement Home page with dynamic products and add-product modal

---
+++ src/component/Modal.jsx
@@ -0,0 +1,145 @@
+import { X } from "lucide-react";
+import { useProductForm } from "../hooks/useProductForm";
+
+const Modal = ({ setOpen, onAdd, onClose }) => {
+  const { form, handleOnChange, handleSubmit } = useProductForm(onAdd, onClose);
+
+  return (
+    <div className="max-w-lg w-full mx-auto bg-white p-8 rounded-2xl shadow-xl border border-gray-100">
+      <div className="mb-6 flex justify-between items-center gap-2">
+        <h2 className="text-2xl font-semibold text-gray-800 ">
+          Add New Product
+        </h2>
+        <div className="cursor-pointer">
+          <X onClick={() => setOpen(false)} size={25} />
+        </div>
+      </div>
+      <form className="space-y-4">
+        <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-600">
+            Feature Image
+          </label>
+          <input
+            type="file"
+            name="image"
+            accept="image/*"
+            onChange={handleOnChange}
+            className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-600">
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleOnChange}
+            placeholder="Enter product name"
+            className="w-full border m.border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-600">Category</label>
+          <input
+            type="text"
+            name="category"
+            value={form.category}
+            onChange={handleOnChange}
+            placeholder="e.g. Electronics"
+            className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+            required
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-600">
+            Description
+          </label>
+          <textarea
+            name="description"
+            placeholder="Enter a short description..."
+            value={form.description}
+            onChange={handleOnChange}
+            rows="3"
+            className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition resize-none"
+            required
+          />
+        </div>
+
+        {/* <div className="space-y-1">
+          <label className="text-sm font-medium text-gray-600">
+            Specification
+          </label>
+          <textarea
+            name="specification"
+            placeholder="Enter specifications..."
+            rows="3"
+            className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition resize-none"
+            required
+          />
+        </div> */}
+
+        <div className="grid grid-cols-3 gap-3">
+          <div className="space-y-1">
+            <label className="text-sm font-medium text-gray-600">Rating</label>
+            <input
+              type="number"
+              name="rating"
+              value={form.rating}
+              onChange={handleOnChange}
+              min={0}
+              placeholder="0–5"
+              className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+              required
+            />
+          </div>
+          <div className="space-y-1">
+            <label className="text-sm font-medium text-gray-600">Price</label>
+            <input
+              type="number"
+              name="price"
+              value={form.price}
+              onChange={handleOnChange}
+              placeholder="₱0.00"
+              min={0}
+              className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+              required
+            />
+          </div>
+          <div className="space-y-1">
+            <label className="text-sm font-medium text-gray-600">
+              Quantity
+            </label>
+            <input
+              type="number"
+              name="quantity"
+              value={form.quantity}
+              onChange={handleOnChange}
+              placeholder="0"
+              min={0}
+              className="w-full border border-gray-300 focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none p-2.5 rounded-lg transition"
+              required
+            />
+          </div>
+        </div>
+
+        {/* Button */}
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="w-full bg-pink-600 text-white py-2.5 rounded-lg hover:bg-pink-700 active:scale-[0.98] transition font-medium shadow-md cursor-pointer"
+        >
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,48 @@
-import ProductList from "../component/ProductList";
+import ProductCard from "../component/ProductCard";
 import Layout from "../Layout/Layout";
-import AddProduct from "../component/AddProduct";
+import Modal from "../component/Modal";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import { useProduct } from "../hooks/ProductContext";
+import dynamicProducts from "../hooks/useDynamic";
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const { product, handleAddProduct } = useProduct();
+
+  const combinedProducts = [...dynamicProducts, ...product];
+
   return (
     <Layout>
-      <AddProduct />
-      <ProductList />
+      {/* Modal */}
+      <div className="min-h-screen bg-gray-100 py-10 px-10 md:px-25">
+        {open && (
+          <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
+            <Modal
+              setOpen={setOpen}
+              onAdd={handleAddProduct}
+              onClose={() => setOpen(false)}
+            />
+          </div>
+        )}
+
+        {/* Product card(s) */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 xl:gap-15 place-items-center">
+          {combinedProducts.map((product, index) => (
+            <ProductCard key={index} product={product} setOpen={setOpen} />
+          ))}
+
+          <div
+            onClick={() => setOpen(true)}
+            className="group h-90 w-full bg-white rounded-2xl shadow-sm hover:shadow-xl border border-dashed border-pink-400 transition-all duration-300 flex items-center justify-center cursor-pointer"
+          >
+            <Plus
+              size={40}
+              className="text-pink-500 group-hover:scale-110 group-active:scale-100 transition"
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
